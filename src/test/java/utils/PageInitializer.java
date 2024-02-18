13dd9bc9ea4d1a3343b14fc1ce4a3c64a3f96042
Swatiru <swatimbi@gmail.com>
2024-02-17 21:30:19 -0500

    my first cucunber frame work

---
+++ src/test/java/utils/PageInitializer.java
@@ -0,0 +1,27 @@
+package utils;
+import pages.AddEmployeePage;
+import pages.EmployeeSearchPage;
+import pages.LoginPage;
+
+import pages.DashboardPage;
+
+
+public class PageInitializer {
+
+    public static LoginPage login;
+    public static DashboardPage dashboardPage;
+    public static AddEmployeePage addEmployeePage;
+    public static EmployeeSearchPage employeeSearchPage;
+
+
+
+
+    public static void initializePageObjects(){
+        login = new LoginPage();
+        dashboardPage = new DashboardPage();
+        addEmployeePage = new AddEmployeePage();
+        employeeSearchPage = new EmployeeSearchPage();
+    }
+
+
+}
